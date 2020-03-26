80b34ca19a74fe1cf994bacf3aead37b39f06cb5
Kamsi <kamsiokeke8242@gmail.com>
2020-03-26 21:19:34 +0000

    This is my first python upload

---
+++ spare.py
@@ -0,0 +1,15 @@
+def home_workout():
+    arms = ["press-ups"]
+    back = ["delt raises"]
+    chest = ["press-ups"]
+    abs = ["ab-wheel roll-out", "plank", "side-planks"]
+    legs = ["heel raises", "pistol squats", "hip thrusts"]
+    cardio = ["jogging", "skipping"]
+    duration = [" reps"]
+    cheat_day == "Do nothing"
+    if day == Monday:
+        print(cheat_day)
+
+
+class weekday(object):
+    input("day of the week")
